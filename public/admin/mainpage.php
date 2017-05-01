eb9739f7bab6bd45eabe3cbf7f1d88189947eaf7
mazgajmaciek <mazgajmaciek@gmail.com>
2017-05-01 08:06:38 +0200

    building main page

--- public/admin/mainpage.php
+++ public/admin/mainpage.php
@@ -1,3 +1,12 @@
+
+<h2>Dodaj nową wiadomość: </h2>
+        <form method="post">
+            <?php //echo join('<br>', $errors); ?>
+            <br>
+            Twoja wiadomosc <input name="message_text" placeholder="Maksymalnie 140 znakow" maxlength="140">
+            <br>
+            <button type="submit">Wyslij</button>
+
 <?php
 include_once '../bootstrap.php';
 //SHOW * FROM Messages of all users, sorted by date when added
@@ -6,14 +15,23 @@ var_dump($_SESSION);
 
 $sql = "SELECT Users.username, Messages.message_text, Messages.message_datetime FROM Messages JOIN Users ON Users.id=Messages.user_id ORDER BY Messages.message_datetime DESC";
 
+$result = $connection->query($sql);
 
+    if ($result->rowCount() > 0) {
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            echo $row['message_text'] . '<br>';
+            echo $row['username'] . '<br>';
+            echo $row['message_datetime'] . '<br>';
+            echo '<br>';
+            
+        }
+    }
+    
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+        if (isset($_POST['message_text'])) {
+            
+        }
+    }
 
 ?>
 
-<h2>Dodaj nową wiadomość: </h2>
-        <form method="post">
-            <?php echo join('<br>', $errors); ?>
-            <br>
-            Twoja wiadomosc <input name="message_text">
-            <br>
-            <button type="submit">Wyslij</button>
