92c15d6bce258810721aef520368344b49d48419
Maciek <mazgajmaciek@gmail.com>
2017-11-05 23:52:15 +0100

    changed bootstrap formatting

--- public/autoload.php
+++ public/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 
-// DIR - w momencie wywowalania wskazuje sciezke na aktualny folder w ktorym
+// DIR - w momencie wywolania wskazuje sciezke na aktualny folder w ktorym
 // zostal wywolany
 
 include_once __DIR__ . '/../src/User.php';
