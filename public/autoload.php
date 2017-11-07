2915d1a4565b7831c5ae697764abdfea0586da5d
Maciek <mazgajmaciek@gmail.com>
2017-11-08 00:03:01 +0100

    starting refactoring in preparation for switching to rest

--- public/autoload.php
+++ public/autoload.php
@@ -4,7 +4,7 @@
 // DIR - w momencie wywolania wskazuje sciezke na aktualny folder w ktorym
 // zostal wywolany
 
-include_once __DIR__ . '/../src/User.php';
-include_once __DIR__ . '/../src/Tweet.php';
-include_once __DIR__ . '/../src/Comment.php';
-include_once __DIR__ . '/../src/PrivateMessage.php';
+include_once __DIR__ . '/../rest/class/User.php';
+include_once __DIR__ . '/../rest/class/Tweet.php';
+include_once __DIR__ . '/../rest/class/Comment.php';
+include_once __DIR__ . '/../rest/class/PrivateMessage.php';
