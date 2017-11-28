3282846c198d0322da189702e638ced7851e3ea4
Maciek <maciek@student.edu>
2017-11-28 23:47:17 +0100

    registration form needs to be checking if username was provided

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -35,39 +35,73 @@ $(function () {
                     $loginResult.slideToggle().delay(500);
 
                     setTimeout(' window.location.href = "mainpage.html"; ',500);
-                    return false;
                 } else {
                     var msg = `<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> ${response.error} </div>`;
                     $loginResult.html(msg);
                     $loginResult.slideDown().delay(500);
                     $loginResult.slideUp().delay(500);
-                    //$loginResult.hide();
 
-
-                    // $loginResult.add('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span>   '+response.error+'</div>');
-                    // $loginResult.show();
-                    // $loginResult.delay(1000).fadeOut("normal", function() {
-                    //
-                    // });
-                    // this.remove();
                     return false;
                 }
             })
             .fail(function (response) {
             console.log(response.error);
             });
+    });
 
+    var $registerPopup = $("#registerPopup");
+
+    $registerPopup.on('submit', function (event) {
+        event.preventDefault();
+
+        var that = $(this),
+            url = that.attr('action'),
+            type = that.attr('method'),
+            data = {};
+
+        that.find('[name]').each(function () {
+
+            var that = $(this),
+                name = that.attr('name'),
+                value = that.val();
+
+            data[name] = value;
+        });
+
+        $.ajax({
+            url: url,
+            type: type,
+            data: data,
+            dataType: 'json'
+        })
+            .done(function (response) {
+                console.log(response);
+
+                var $regResult = $("#regResult");
+                var that = $(this);
+
+                if(response.success){
+                    var msg = `<div class="alert alert-success"> <span class="glyphicon glyphicon-info-sign"></span> ${response.success} </div>`;
+                    $regResult.html(msg);
+                    $regResult.slideToggle().delay(500);
+
+                    setTimeout(' window.location.href = "mainpage.html"; ',500);
+                } else {
+                    var msg = `<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> ${response.error} </div>`;
+                    $regResult.html(msg);
+                    $regResult.slideDown().delay(500);
+                    $regResult.slideUp().delay(500);
+
+                    return false;
+                }
+            })
+            .fail(function (response) {
+                console.log(response.error);
+            });
 
     });
 
 
-});
 
-    // $('body').on('submit', '#modalRegister', function (element) {
-    //     element.preventDefault();
-    //
-    //
-    //     var $username = $('#modalRegister').find('#username').val();
-    //     alert($username);
-    //
-    // });
+
+});
